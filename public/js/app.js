1e5780d0952bb03acabd74eccab12555635628cd
Sittiphat <sittiphat@g.ucla.edu>
2021-10-24 00:35:56 -0700

    Added Express.js server

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+let model;
+
+(async function() {
+    model = await tf.loadModel("http://localhost:3000/tfjs_files/model.json");
+    console.log(model);
+})();
--- src/app.js
+++ src/app.js
@@ -1,179 +1,63 @@
-// Core module first
-const path = require("path");
 const express = require("express");
-const hbs = require("hbs");
-const geocode = require("./utility/geocode")
-const forecast = require("./utility/forecast")
-const pollution = require("./utility/pollution")
+const cors = require('cors');
+const path = require("path");
+// const translateText = require("../utility/translate.js");
+// const transformText = require("../utility/transformer.js");
 
-// // Variable that shows path, but we can just use paths
-// console.log(__dirname);
-// console.log(__filename);
-// This core module is navigating from relative path
-console.log(path.join(__dirname, "../public"));
-
-// Express is a function
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-// Define Paths for Express Configs
+app.use(express.text());
+app.use(express.json());
+app.use(cors());
+
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
+const mlDirectoryPath = path.join(__dirname, "../ml");
+console.log(publicDirectoryPath);
 
-// Setuo handle bar engine and views location
-// We can see documentation in Express API Website
-// Setting up Express Handle Bars for Dynamic Templates using hbs
-// It is built into express so hbs not needed yet
-app.set("view engine", "hbs");
-// // If we choose to rename views folder
-app.set("views", viewsPath);
-hbs.registerPartials(partialsPath);
-
-
-// Setup Static directory to serve
-// A way to customize the server
-// Pulls HTML and the express.static searches this file then return
 app.use(express.static(publicDirectoryPath));
+app.use(express.static(mlDirectoryPath));
 
-// Using to render one of our views
-app.get("", (req, res) => {
-    // Name of the file then all the objects you want view to access
-    res.render("index.hbs", {
-        title: "Pollution App",
-        name: "Jesse Narkmanee"
-    });
-});
+// http://localhost:3000/tfjs_files/model.json
 
-// Will not run unless not found but should be found
-// index.html is special so no need to put anything
-app.get("", (req, res) => {
-    res.send("Hello express!");
-});
-
-app.get("/info", (req, res) => {
-    res.render("help.hbs", {
-        title: "More Info",
-        msg: "Hello there",
-        name: "Jesse Narkmanee"
-    });
-})
-
-app.get("/info/*", (req, res) => {
-  res.render("404.hbs",{
-      title: "404 Page",
-      name: "Jesse Narkmanee",
-      errorMessage: "More Info Article not found!"
-  });
-})
-
-app.get("/help/*", (req, res) => {
-    res.send("Help article not found!");
-})
-
-app.get("/help", (req, res) => {
-  res.send([{name: "Golf"}, {name: "Jesse"}]);
-});
-
-// Using Handle bars instead of hard coding then using .html
-app.get("/about", (req, res) => {
-  res.render("about.hbs",{
-      title: "About Pollution",
-      name: "Jesse Narkmanee"
-  });
-});
-
-app.get("/about", (req, res) => {
-    res.send("<h1>About<h1>");
-});
-
-// http://api.openweathermap.org/data/2.5/air_pollution?lat=27.2046&lon=77.4977&appid=f2766319db1b425a161fd09bd1549238
-
-// app.get("/pollution", (req, res) => {
-//     if (!req.query.address) {
-//         return res.send({
-//             error: "You must provide a location address"
-//         });
-//     }
-
-// })
-
-app.get("/weather", (req, res) => {
-    // Usually we send the HTML title
-    if (!req.query.address) {
-        return res.send({
-            error: "You must provide an address. This can be a full home address, city, or zip code."
-        });
-    }
-
-    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
-        if (error) {
-            return res.send({error});
-        }
-
-
-
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({error});
-            }
-
-            // res.send({
-            //     forecast: forecastData,
-            //     location,
-            //     address: req.query.address
-            // });
-            pollution(latitude, longitude, (error, pollutionData) => {
-                if (error) {
-                    return res.send({error});
-                }
-
-                res.send({
-                    forecast: forecastData,
-                    pollution: pollutionData,
-                    location,
-                    address: req.query.address
-                });
-        });
-        });
-    });
-
-    // res.send({
-    //     forecast: "Snowing",
-    //     location: "Philadelphia",
-    //     address: req.query.address
-    // });
-});
-
-app.get("/products", (req, res) => {
-    if (!req.query.search) {
-        // Do not send double sends
-        return res.send({
-            error: "You must provide a search term"
-        })
-    }
-    console.log(req.query);
-    res.send({
-        products: []
-    });
-})
-
+// If wrong link is provided instructions are provided below
 app.get("*", (req, res) => {
-  res.render("404.hbs",{
-      title: "404 Page",
-      name: "Jesse Narkmanee",
-      errorMessage: "Page not found!"
-  });
+    res.send("Default statically rendered page");
 });
 
-// app.get('*', (req, res) => {
-//     res.send("My 404 page")
-// })
+// app.get("/", (req, res) => {
+//     res.render("main.html");
+// });
 
-// app.com
-// app.com/help
-// app.com/about
+// app.post("/translate-ch", (req, res) => {
 
+//     translateText(req.body, "zh").then(t_t => res.send(t_t));
+
+// });
+
+// app.post("/translate-sp", (req, res) => {
+
+//     translateText(req.body, "es").then(t_t => res.send(t_t));
+// });
+
+// // This is the main API endpoint used in the final version. We transform 
+// // the text by calling our helper function defined in our self-made import module.
+// app.post("/transform", (req, res) => {
+
+//     res.send(transformText(req.body));
+// });
+
+// app.post("/transform-translate-ch", (req, res) => {
+
+//     translateText(transformText(req.body), "zh").then(t_t => res.send(t_t));
+// });
+
+// app.post("/transform-translate-sp", (req, res) => {
+
+//     translateText(transformText(req.body), "es").then(t_t => res.send(t_t));
+// });
+
+// Server listens to incoming request.
 app.listen(port, () => {
     console.log(`Server is up on port ${port}.`);
 });
